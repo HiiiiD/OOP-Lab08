125da19bb4a8015cac79dde2c717f5f1a24de0d6
HiD <salomone.marco@outlook.it>
2020-11-16 12:24:29 +0100

    Finished fourth exercise

--- src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
+++ src/it/unibo/oop/lab/mvcio2/SimpleGUIWithFileChooser.java
@@ -1,13 +1,33 @@
 package it.unibo.oop.lab.mvcio2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+import it.unibo.oop.lab.mvcio.Controller;
+
 /**
  * A very simple program using a graphical interface.
  * 
  */
 public final class SimpleGUIWithFileChooser {
 
+    private final JFrame frame = new JFrame();
+
+    private final Controller controller = new Controller();
+
     /*
-     * TODO: Starting from the application in mvcio:
      * 
      * 1) Add a JTextField and a button "Browse..." on the upper part of the
      * graphical interface. Suggestion: use a second JPanel with a second
@@ -31,5 +51,98 @@ public final class SimpleGUIWithFileChooser {
      * the UI: in this example the UI knows when should be updated, so try to keep
      * things separated.
      */
+
+    /**
+     * Build a simple GUI with a file chooser with the MVC pattern.
+     */
+    public SimpleGUIWithFileChooser() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+        /*
+         * Instead of appearing at (0,0), upper left corner of the screen, this flag
+         * makes the OS window manager take care of the default positioning on screen.
+         * Results may vary, but it is generally the best choice.
+         */
+        frame.setLocationByPlatform(true);
+
+        /**
+         * Create the main content pane
+         */
+        final JPanel mainContentPane = new JPanel();
+        final BorderLayout mainContentPaneLayout = new BorderLayout();
+        mainContentPane.setLayout(mainContentPaneLayout);
+
+        final JTextArea textArea = new JTextArea();
+        mainContentPane.add(textArea, BorderLayout.CENTER);
+        final JButton saveButton = new JButton("Save");
+        mainContentPane.add(saveButton, BorderLayout.SOUTH);
+
+        /*
+         * Create the north panel for browsing files 
+         */
+        final JPanel browsePanel = new JPanel();
+        /**
+         * Create a textfield that is non-modifiable
+         */
+        final JTextField browseTextField = new JTextField(controller.getCurrentFilePath());
+        browseTextField.setEditable(false);
+        final JButton browseFilesButton = new JButton("Browse...");
+        browsePanel.setLayout(new BorderLayout());
+        browsePanel.add(browseTextField, BorderLayout.CENTER);
+        browsePanel.add(browseFilesButton, BorderLayout.LINE_END);
+
+        /*
+         * Open a {@link JFileChooser} on click
+         */
+        browseFilesButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                /**
+                 * Create a file chooser with the default file set as the {@link Controller.getCurrentFile()}
+                 */
+                final JFileChooser fileChooser = new JFileChooser(controller.getCurrentFile());
+                final int dialogResult = fileChooser.showSaveDialog(browsePanel);
+                if (dialogResult == JFileChooser.APPROVE_OPTION) {
+                    controller.setFile(fileChooser.getSelectedFile());
+                    browseTextField.setText(controller.getCurrentFilePath());
+                } else if (dialogResult != JFileChooser.CANCEL_OPTION) {
+                    JOptionPane.showMessageDialog(browsePanel, "Error occurred");
+                    System.out.println(dialogResult);
+                }
+            }
+        });
+
+        mainContentPane.add(browsePanel, BorderLayout.NORTH);
+
+        /*
+         * Save button handler
+         */
+        saveButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.saveContent(textArea.getText());
+                } catch (IOException e1) {
+                    JOptionPane.showMessageDialog(frame, e1, "File error", JOptionPane.ERROR_MESSAGE);
+                    e1.printStackTrace();
+                }
+            }
+
+        });
+
+        frame.setContentPane(mainContentPane);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+
+    public static void main(final String[] args) {
+        new SimpleGUIWithFileChooser();
+    }
 
 }
