abe66f8541e3299a789a5311d20416d1bd432682
HiD <salomone.marco@outlook.it>
2020-11-16 11:02:08 +0100

    Finished code analysis exercise

--- src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
+++ src/it/unibo/oop/lab/codeanalysis/ThisIsHowThingsShouldNotBeDone.java
@@ -10,12 +10,11 @@ import java.util.Collection;
  * This code triggers static code analyzers. You should use it to see how things
  * SHOULD NOT be done.
  *
- * @author Danilo Pianini
- * @version 1.2
- *
  */
-public class ThisIsHowThingsShouldNotBeDone {
+public final class ThisIsHowThingsShouldNotBeDone {
 
+    private ThisIsHowThingsShouldNotBeDone() {
+    }
     /*
      * PMD complains:
      * 
@@ -43,13 +42,13 @@ public class ThisIsHowThingsShouldNotBeDone {
      * 
      * Missing Javadoc
      */
-    public static void main(String[] a) {
+    public static void main(final String[] a) {
         /*
          * PMD Complains:
          * 
          * this variable should be final!
          */
-        Collection<Object> c = new ArrayList<>();
+        final Collection<Object> c = new ArrayList<>();
         /*
          * PMD complains:
          * 
@@ -61,14 +60,16 @@ public class ThisIsHowThingsShouldNotBeDone {
          * 
          * What does it mean "43"? It's a magic number!
          */
-        for (int i = 0; i < 43; i++)
+        final int elements = 43;
+        for (int i = 0; i < elements; i++) {
             c.add(new Object());
+        }
         /*
          * FindBugs complains
          * 
          * Slow: use clear() instead!
          */
-        c.removeAll(c);
+        c.clear();
     }
 
 }
